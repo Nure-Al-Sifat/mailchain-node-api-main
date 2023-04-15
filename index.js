ec08871d891b7477f8595ac2d784c30aac291ba1
Nure Al Sifat <75719853+Nure-Al-Sifat@users.noreply.github.com>
2023-04-15 17:36:18 +0600

    .

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const corsOptions = {
   origin: "http://localhost:3000",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
+  methods: "GET, POST",
 };
 app.use(cors(corsOptions));
 

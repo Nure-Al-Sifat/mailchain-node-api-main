3397141131949e858043e45e49e6840ec26bef45
Nure Al Sifat <75719853+Nure-Al-Sifat@users.noreply.github.com>
2023-04-15 21:20:19 +0600

    .

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // app.use(cors());
 
 const corsOptions = {
-  origin: "*",
+  origin: "https://mailchain-node-api-main-production.up.railway.app",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };

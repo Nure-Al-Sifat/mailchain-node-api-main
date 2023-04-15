ee9baafbbed2220788bd46614b35c278e998a903
Nure Al Sifat <75719853+Nure-Al-Sifat@users.noreply.github.com>
2023-04-15 17:29:25 +0600

    .

--- index.js
+++ index.js
@@ -8,16 +8,14 @@ const schedule = require("node-schedule");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
-// app.use(
-//   cors({
-//     origin: "*",
-//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     preflightContinue: false,
-//     optionsSuccessStatus: 204,
-//   })
-// );
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 //OPEN AI CONFIG
 const configuration = new Configuration({
